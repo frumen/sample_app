1136d055e459729ea47c1e850a795e5ad367a367
Federico D. Frumento <federicofrumento21@hotmail.com>
2012-05-12 15:01:05 -0300

    Add title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  
   describe "Home page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
     	page.should have_selector('h1', :text => 'Sample App')
     end
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                  :text => "Ruby on Rails Tutorial Sample App | Home")
+                                  :text => "#{base_title}")
     end
+    it "should not have a custom title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                                  :text => "| Home")
+    end    
   end
 
   describe "Help Page" do
@@ -21,7 +29,7 @@ describe "StaticPages" do
         it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                  :text => "Ruby on Rails Tutorial Sample App | Help")
+                                  :text => "#{base_title} | Help")
     end
   end
   
@@ -33,7 +41,7 @@ describe "StaticPages" do
         it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                  :text => "Ruby on Rails Tutorial Sample App | About Us")
+                                  :text => "#{base_title} | About Us")
     end
   end
   
@@ -45,7 +53,7 @@ describe "StaticPages" do
         it "should have the right title" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                                  :text => "Ruby on Rails Tutorial Sample App | Contact")
+                                  :text => "#{base_title} | Contact")
     end
   end
   
