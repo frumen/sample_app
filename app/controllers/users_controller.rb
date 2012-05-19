1ecf2789d6a8087380046081fd122559cdd1d217
Federico D. Frumento <federicofrumento21@hotmail.com>
2012-05-19 19:52:00 -0300

    Permisos de usuarios1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -17,5 +20,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Tenes algo diferente!"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Primero loggeate loco!."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
   
 end
