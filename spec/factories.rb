be4997513d4e04e9d58b16373da38f5aa37a5e4c
Federico D. Frumento <federicofrumento21@hotmail.com>
2012-05-19 11:28:40 -0300

    Signup completo!

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     name     "Michael Hartl"
     email    "michael@example.com"
-    password "foobar"
-    password_confirmation "foobar"
+    password "foobar11"
+    password_confirmation "foobar11"
   end
 end
