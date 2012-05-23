74db657f4001e98b1c54ff8b8f914129835d1282
Federico D. Frumento <federicofrumento21@hotmail.com>
2012-05-23 20:13:12 -0300

    Renovaciones

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_back_or user
+      #redirect_back_or user --antes te llevaba a la pag de perfil del usuario 
+      redirect_to root_path
     else
       flash.now[:error] = 'Pifiaste! Pone las cosas bien, pedazo de pastrami!'
       render 'new'
